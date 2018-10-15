5a47061a9edd29c4b0a89fa4d3bb582ca127076f
Rene van Vliet <r.vanvliet@student.fontys.nl>
2018-10-15 15:27:32 +0200

    Added Allrooms/all and Allrooms/realtime route

--- API/socket_reading/urls.py
+++ API/socket_reading/urls.py
@@ -23,11 +23,17 @@ from . import views
 router = routers.DefaultRouter()
 router.register('socket_readings', views.Socket_ReadingViewSet)
 router.register(r'(?P<identifier>[0-9]*)/realtime',views.Socket_ReadingRoomIdRealtimeViewSet,base_name="realtime")
-router.register(r'(?P<identifier>.+)/realtime',views.Socket_ReadingRoomRoomnameRealtimeViewSet,base_name="realtime")
+router.register(r'(?P<identifier>[0-9]*)/realtime',views.Socket_ReadingRoomRoomnameRealtimeViewSet,base_name="realtime")
 
 router.register(r'(?P<identifier>[0-9]*)/all',views.Socket_ReadingRoomIdAllViewSet,base_name="all")
-router.register(r'(?P<identifier>.+)/all',views.Socket_ReadingRoomRoomnameAllViewSet,base_name="all")
+router.register(r'(?P<identifier>[0-9]*)/all',views.Socket_ReadingRoomRoomnameAllViewSet,base_name="all")
+
+
+router.register('cleanOld',views.Socket_ReadingCleanOldViewSet,base_name="clean")
+
+router.register('Allrooms/realtime',views.Socket_ReadingAllRoomsRealtimeViewSet,base_name="clean")
+router.register('Allrooms/all',views.Socket_ReadingAllRoomsAllViewSet,base_name="clean")
 
 urlpatterns = [
-	path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- API/socket_reading/views.py
+++ API/socket_reading/views.py
@@ -21,32 +21,33 @@ class Socket_ReadingViewSet(viewsets.ModelViewSet):
     serializer_class = Socket_ReadingSerializer
 
 
-class Socket_ReadingRoomIdRealtimeViewSet(viewsets.ModelViewSet):
 
+
+class Socket_ReadingAllRoomsRealtimeViewSet(viewsets.ModelViewSet):
+    queryset = Socket_Reading.objects.raw('select distinct on(socket_id_id) * from socket_reading_socket_reading order by socket_id_id,reading_time desc;');
     serializer_class = Socket_ReadingSerializer
-    def get_queryset(self):
-        identifier = self.kwargs['identifier']
-        queryset = Socket_Reading.objects.filter(socket_id=identifier).order_by('-id')[:1]
-        return queryset
 
+class Socket_ReadingAllRoomsAllViewSet(viewsets.ModelViewSet):
+    queryset = Socket_Reading.objects.all()
+    serializer_class = Socket_ReadingSerializer
+
+#roomname realtime
 class Socket_ReadingRoomRoomnameRealtimeViewSet(viewsets.ModelViewSet):
     serializer_class = Socket_ReadingSerializer
+
     def get_queryset(self):
         identifier = self.kwargs['identifier']
         room = Room.objects.filter(room_name=identifier).first()
         sockets = Sockets.objects.filter(room_id=room.id)
-        queryset = Socket_Reading.objects.filter(socket_id__in=sockets.values("id")).order_by('-id')[:1]
+        queryset = Socket_Reading.objects.filter(socket_id__in=sockets.values("id")).order_by('-socket_id','-created').distinct("socket_id")
         return queryset
 
-class Socket_ReadingRoomIdAllViewSet(viewsets.ModelViewSet):
-    serializer_class = Socket_ReadingSerializer
-    def get_queryset(self):
-        identifier = self.kwargs['identifier']
-        queryset = Socket_Reading.objects.filter(socket_id=identifier)
-        return queryset
+
+#roomname all
 
 class Socket_ReadingRoomRoomnameAllViewSet(viewsets.ModelViewSet):
     serializer_class = Socket_ReadingSerializer
+
     def get_queryset(self):
         identifier = self.kwargs['identifier']
         room = Room.objects.filter(room_name=identifier).first()
@@ -54,3 +55,28 @@ class Socket_ReadingRoomRoomnameAllViewSet(viewsets.ModelViewSet):
         queryset = Socket_Reading.objects.filter(socket_id__in=sockets.values("id"))
         return queryset
 
+#id realtime
+class Socket_ReadingRoomIdRealtimeViewSet(viewsets.ModelViewSet):
+    serializer_class = Socket_ReadingSerializer
+
+    def get_queryset(self):
+        identifier = self.kwargs['identifier']
+        queryset = Socket_Reading.objects.filter(socket_id=identifier).order_by('-id')[:1]
+        return queryset
+
+#id all
+class Socket_ReadingRoomIdAllViewSet(viewsets.ModelViewSet):
+    serializer_class = Socket_ReadingSerializer
+
+    def get_queryset(self):
+        identifier = self.kwargs['identifier']
+        queryset = Socket_Reading.objects.filter(socket_id=identifier)
+        return queryset
+
+
+class Socket_ReadingCleanOldViewSet(viewsets.ModelViewSet):
+    queryset = Socket_Reading.objects.all()
+    serializer_class = Socket_ReadingSerializer
+
+    def destroy(self, request, *args):
+        pass
