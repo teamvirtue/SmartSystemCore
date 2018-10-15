06362a4f0e6a915477e99c2722319d78f9b0aaf1
Stijn <stijn.groenen@student.fontys.nl>
2018-10-15 11:07:45 +0200

    Added reading time to movement sensor model.

--- API/movement_reading/models.py
+++ API/movement_reading/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class MovementReading(models.Model):
 	sensor_id = models.IntegerField()
 	value = models.IntegerField()
+	reading_time = models.DateTimeField(auto_now_add=True)
